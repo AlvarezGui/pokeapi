0449083ed9a4ef8ebc4b2754f443174a1bde8708
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2022-08-31 10:18:40 -0300

    Add files via upload

---
+++ components/find.jsx
@@ -0,0 +1,44 @@
+import Pokedex from 'pokedex-promise-v2';
+import { useState } from 'react';
+
+export default function Find(){
+
+    const P = new Pokedex();
+
+    //variaveis
+    const [gen, setGen] = useState("");
+    const [numr, setNumr] = useState("");
+    const [erro, setErro] = useState("");
+    
+
+    //pegar as informações da geração (vem da API)
+    const getID = async() => {
+        await P.getGenerationByName(`generation-` + numr)
+        .then((result)=>{
+            setGen(result);
+            setErro("");
+        })
+        .catch((err) =>{
+            setErro("Invalido");
+            console.log(err);
+        });
+       
+    }
+
+    return(
+        <>
+            <h1>Geração: {gen?.name}</h1>
+            <h3>Região: {gen?.main_region?.name ?? ""}</h3>
+            <p>Jogo: {gen?.version_groups?.[0]?.name ?? ""}</p>
+
+            <div>
+                <label>Escolha Uma geração (em numeros romanos)</label> <br />
+                <input type="text" placeholder='Geração' required  onChange={event=>setNumr(event.target.value)} value={numr} /> <br />
+                <input type="button" value="Confirmar" onClick={getID} />
+            </div>
+
+            <h1>{erro}</h1>
+        </>
+    );
+
+}
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Find from '../components/find';
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Find></Find>
+    </>
+  );
+}
